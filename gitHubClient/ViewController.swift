4417beabccd3a8cd0dd29b80e35364fab001acc0
LINDSEY BOGGIO <BLUEHARP2@GMAIL.COM>
2016-06-29 19:08:03 -0700

    Started buildout of LogIn VC

--- gitHubClient/ViewController.swift
+++ gitHubClient/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var logInButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,19 +23,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    @IBAction func requestToken(sender: AnyObject) {
+    
+    @IBAction func logInButtonSelected(sender: AnyObject) {
         GitHubOAuth.shared.oAuthRequestWith(["scope" : "email,user,repo"])
     }
-    
-    @IBAction func printToken(sender: AnyObject) {
-        do{
-            let token = try GitHubOAuth.shared.accessToken()
-            print(token)
-            
-        }catch let error{
-            print(error)
-        }
-    }
-
 }
 
