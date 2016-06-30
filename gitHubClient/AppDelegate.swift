70fe51b9500a38ab722145dd56d9226c175d7b28
LINDSEY BOGGIO <BLUEHARP2@GMAIL.COM>
2016-06-29 19:54:44 -0700

    continued work in AppDelegate

--- gitHubClient/AppDelegate.swift
+++ gitHubClient/AppDelegate.swift
@@ -13,6 +13,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
     var oAuthViewController: OAuthViewController?
+    var homeViewController: HomeViewController?
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
@@ -27,7 +28,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         GitHubOAuth.shared.tokenRequestWithCallback(url, options: SaveOptions.Keychain) { (sucess) in
             if sucess{
                 //print("We have a token!!")
-        
+                if let oAuthViewController = self.oAuthViewController{
+                    UIView.animateWithDuration(0.4, delay: 1.0, options: .CurveEaseInOut, animations: { 
+                        
+                        oAuthViewController.view.alpha = 0.0
+                        
+                        }, completion: { (finished) in
+                            oAuthViewController.view.removeFromSuperview()
+                            oAuthViewController.removeFromParentViewController()
+                            
+                            //self.homeViewController.update()
+                    })
+                }
             }
         }
         return true
@@ -38,15 +50,22 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         do{
             if let token = try GitHubOAuth.shared.accessToken(){ print(token) }
             
-            
-        }catch _{}
+        }catch _{self.presentOAuthViewController()}
     }
 
+    
     func presentOAuthViewController(){
         guard let homeViewController = self.window?.rootViewController as? HomeViewController else{fatalError("Check your Root View Controller")}
         guard let storyboard = homeViewController.storyboard else {fatalError("Check for Storyboard")}
         guard let oAuthViewController = storyboard.instantiateViewControllerWithIdentifier(OAuthViewController.id) as?
             OAuthViewController else {fatalError("Error with OAuthVC")}
+        
+        homeViewController.addChildViewController(oAuthViewController)
+        homeViewController.view.addSubview(oAuthViewController.view)
+        oAuthViewController.didMoveToParentViewController(homeViewController)
+        
+        self.homeViewController = homeViewController
+        self.oAuthViewController = oAuthViewController
         
     }
 }
